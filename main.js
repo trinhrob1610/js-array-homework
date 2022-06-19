37adf97d68badff69b14db2700a220ca8d78b62a
trinh <hoaithanh@MacBook-Air-cua-Hoai.local>
2022-06-19 13:39:56 +0700

    1, 2, 3

--- main.js
+++ main.js
@@ -19,13 +19,42 @@ function bai1(n){
 bai1(100);
 
 
-function bai2(arr){
-    for(var i = 0; i < arr.length;i++){
-        console.log(i);
-    }
-}
-arr([1, 4, 3, 1, 4, 4]);
-bai2();
+function bai2(n)
+  {
+      // Biến cờ hiệu
+      var flag = true;
+
+      // Nếu n bé hơn 2 tức là không phải số nguyên tố
+      if (n < 2){
+          flag = false;
+      }
+      else{
+          // lặp từ 2 tới n-1
+          for (var i = 2; i < n-1; i++)
+          {
+              if (n % i == 0){
+                  flag = false;
+                  break;
+              }
+          }
+      }
+
+      // Kiểm tra biến flag
+      if (flag == true){
+          document.write(n + " là số nguyên tố <br/>");
+      }
+      else{
+          document.write(n + " không phải là số nguyên tố <br/>");
+      }
+  }
+
+ bai2(5);
+ bai2(2);
+ bai2(8);
+ bai2(7);
+ bai2(10);
+
+
 
 function tinhTong(){
     var S = 0;
